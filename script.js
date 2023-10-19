fd40a20d8f9fbf3e8208962a2546680be688b17c
Raouf Atabeg <raouf.ilkay@gmail.com>
2023-10-19 18:35:48 -0400

    updated website
    mobile friendly and adaptable version 2.0

--- script.js
+++ script.js
@@ -1,145 +1,17 @@
+
+
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Backend Developer", "Software developer",'Linguist enthusiast'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+  strings: ["Frontend Developer", "Backend Developer", "Software developer",'Linguist enthusiast'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
 })
 
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
 
-window.addEventListener("scroll", reveal);
-
-// To check the scroll position on page load
-reveal();
-
-
-// ——————————————————————————————————————————————————
-// TextScramble
-// ——————————————————————————————————————————————————
-
-class TextScramble {
-    constructor(el) {
-      this.el = el
-      this.chars = '!<>-_\\/[]{}—=+*^?#________'
-      this.update = this.update.bind(this)
-    }
-    setText(newText) {
-      const oldText = this.el.innerText
-      const length = Math.max(oldText.length, newText.length)
-      const promise = new Promise((resolve) => this.resolve = resolve)
-      this.queue = []
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || ''
-        const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 40)
-        const end = start + Math.floor(Math.random() * 40)
-        this.queue.push({ from, to, start, end })
-      }
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0
-      this.update()
-      return promise
-    }
-    update() {
-      let output = ''
-      let complete = 0
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i]
-        if (this.frame >= end) {
-          complete++
-          output += to
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
-          }
-          output += `<span class="dud">${char}</span>`
-        } else {
-          output += from
-        }
-      }
-      this.el.innerHTML = output
-      if (complete === this.queue.length) {
-        this.resolve()
-      } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
-      }
-    }
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)]
-    }
-  }
-  
-  // ——————————————————————————————————————————————————
-  // Example
-  // ——————————————————————————————————————————————————
-  
-//   const phrases = [
-//     'Neo,',
-//     'sooner or later',
-//     'you\'re going to realize',
-//     'just as I did',
-//     'that there\'s a difference',
-//     'between knowing the path',
-//     'and walking the path'
-//   ]
-
-// function title_effects(){
-  
-//   const el = document.querySelector('title')
-//   const fx = new TextScramble(el)
-//   phrases  = document.getElementsByClassName('title').textContent.split(" ")
-//   console.log(phrases)
-  
-//   let counter = 0
-//   const next = () => {
-//     fx.setText(phrases[counter]).then(() => {
-//       setTimeout(next, 800)
-//     })
-//     counter = (counter + 1) % phrases.length
-//   }
-// }
-
-
-
-
-
-  const phrases = [
-    
-    'Computer Science Fundamentals',
-    'Introduction to Computer Organization and Architecture',
-    'Data Structures and Algorithms',
-    'Software Tools and Systems Programming',
-    'Operating Systems',
-    'Databases',
-    'Analysis of Algorithms'
-
-  ]
-
-
-  const el = document.querySelector('.text')
-  const fx = new TextScramble(el)
-  
-  let counter = 0
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 800)
-    })
-    counter = (counter + 1) % phrases.length
-  }
-  
-  next()
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
