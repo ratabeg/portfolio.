8e2b121b5d9542699ed9b2c5b2097138d6e2b1e5
Raouf Atabeg <raouf.ilkay@gmail.com>
2023-11-11 22:39:59 -0500

    updated
    Added animations and update skillsets

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("popup-animation");
+    } else {
+      reveals[i].classList.remove("popup-animation");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
